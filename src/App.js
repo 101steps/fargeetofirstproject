a96ce3682194c3a3e1f4be6627048422ad3c883a
Saadaq <snuur101@gmail.com>
2022-01-20 23:48:15 +0100

    changestwo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        BREAKING CHANGES!
+        waa is badelka cusub!
       </header>
     </div>
   );
